8464975f6198ff4a5fa276938f7cf296d2b2c9f1
Dmitry Selivanov <diselivanov@gmail.com>
2021-12-02 01:05:50 +0300

    Problem 1
    * Used sliding window based solution. May fail for large input. Better to keep in memory only required on current step data.

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,10 +1,22 @@
+private const val SLIDING_WINDOW_SIZE = 3
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input
+            .asSequence()
+            .map { it.toInt() }
+            .windowed(2)
+            .count { pair -> pair[0] < pair[1] }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input
+            .asSequence()
+            .map { it.toInt() }
+            .windowed(SLIDING_WINDOW_SIZE)
+            .map { window -> window.sum() }
+            .windowed(2)
+            .count { pair -> pair[0] < pair[1] }
     }
 
     // test if implementation meets criteria from the description, like:
